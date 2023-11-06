c8488930834eec8de9c33eee28d5470ae9409c7f
Max Tyshkevich <2842778@gmail.com>
2023-11-07 01:16:17 +0300

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,90 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/hilink-logo.svg';
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/contants';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col justify-center gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/">
+            <Image src={logo} alt={'logo'} className="mb-10" />
+          </Link>
+          <div className="flex flex-wrap justify-center gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map(({ links, title }) => (
+              <FooterLink title={title} key={title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {links.map((link) => (
+                    <li key={link}>
+                      <Link href="#">{link}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </FooterLink>
+            ))}
+
+            <div>
+              <FooterLink title={FOOTER_CONTACT_INFO.title}>
+                <ul>
+                  {FOOTER_CONTACT_INFO.links.map(({ label, value }) => (
+                    <li key={label}>
+                      <Link
+                        href="#"
+                        className="flex gap-4 md:flex-col lg:flex-row"
+                      >
+                        <p className="whitespace-nowrap">{label}</p>
+                        <p className="medium-14 whitespace-nowrap text-blue-70">
+                          {value}
+                        </p>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </FooterLink>
+            </div>
+
+            <div>
+              <FooterLink title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => (
+                    <li key={link}>
+                      <Link href="#">
+                        <Image
+                          src={link}
+                          alt="Social logo"
+                          width={25}
+                          height={25}
+                        />
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </FooterLink>
+            </div>
+          </div>
+        </div>
+
+        <div className="border bg-gray-20 " />
+
+        <p className="regular-14 w-full text-center text-gray-30">
+          {new Date().getFullYear()} Hilink | All right reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
+
+type PropsFooterLink = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterLink = ({ title, children }: PropsFooterLink) => (
+  <div className="flex flex-col gap-5">
+    <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+    {children}
+  </div>
+);
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,36 +6,38 @@ import Button from './Button';
 
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src={logo} alt={'logo'} />
-      </Link>
+    <header>
+      <nav className="flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+          <Image src={logo} alt={'logo'} />
+        </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map(({ key, href, label }) => (
-          <Link
-            key={key}
-            href={href}
-            className="regular-16 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {label}
-          </Link>
-        ))}
-      </ul>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map(({ key, href, label }) => (
+            <Link
+              key={key}
+              href={href}
+              className="regular-16 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {label}
+            </Link>
+          ))}
+        </ul>
 
-      <div className="lg:flexCenter hidden">
-        <Button
-          type={'button'}
-          title={'login'}
-          icon={'/user.svg'}
-          variant={'btn_dark_green'}
-        />
-      </div>
+        <div className="lg:flexCenter hidden">
+          <Button
+            type={'button'}
+            title={'login'}
+            icon={'/user.svg'}
+            variant={'btn_dark_green'}
+          />
+        </div>
 
-      <button className="lg:hidden">
-        <Image src={'/menu.svg'} height={32} width={32} alt="menu" />
-      </button>
-    </nav>
+        <button className="lg:hidden">
+          <Image src={'/menu.svg'} height={32} width={32} alt="menu" />
+        </button>
+      </nav>
+    </header>
   );
 };
 
